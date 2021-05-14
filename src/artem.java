903fd2eff72ba44e9766d52f1950a7993213ecb8
Artem <kolaguin@gmail.com>
2021-05-14 11:17:16 +0300

    second commit

--- src/artem.java
+++ src/artem.java
@@ -2,7 +2,7 @@ public class artem {
     public static void main(String[] args) {
         int money = 2000000000;
         int transfer = 500000000;
-        money -= transfer;
+        money += transfer;
         System.out.println(money);
     }
 }
